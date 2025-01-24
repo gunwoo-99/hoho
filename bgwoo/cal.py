b196255880b9709433854f97b19b1d62f3345d3e
gunwoo-99 <bgwoo99@gmail.com>
2025-01-25 00:11:33 +0900

    works which  i did today

---
+++ bgwoo/cal.py
@@ -0,0 +1,24 @@
+# 계산기를 만들거임
+class Calculator():
+    def __init__(self):
+        pass
+
+# 더하기
+    def plus(self, num1, num2):
+        result = num1 + num2
+        return result
+    
+# 뺴기
+    def minus(self, num1, num2):
+        result = num1 - num2
+        return result
+
+# 곱하기
+    def multiply(self, num1, num2):
+        result = num1 * num2
+        return result
+
+# 나누기
+    def divide(self, num1, num2):
+        result = num1 / num2
+        return result
